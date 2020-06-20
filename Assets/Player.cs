50c3e3da7b5b526471fa85cbf1ce34dc8b96bc4a
kexiang <kexiang@thinkplay.co>
2020-06-20 13:22:18 +0800

    update

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : SyncBehavior
+{
+    public override void OnSyncAwake()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnSyncStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnSyncUpdate(float dt)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnSyncDestroy()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/SyncBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SyncBehavior : MonoBehaviour
+{
+    public abstract void OnSyncAwake();
+
+    public abstract void OnSyncStart();
+
+    public abstract void OnSyncUpdate(float dt);
+
+    public abstract void OnSyncDestroy();
+}
